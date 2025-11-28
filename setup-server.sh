2d1816dd3edfceec20af7777ae7da60185b858bb
msi <thatnaing336@gmail.com>
2025-11-28 16:27:10 +0630

    php 8.4  error fix

--- setup-server.sh
+++ setup-server.sh
@@ -17,11 +17,11 @@ sudo add-apt-repository ppa:ondrej/php -y
 
 # Prompt for PHP version
 echo "Available PHP versions:"
-echo "8.0, 8.1, 8.2"
+echo "8.0, 8.1, 8.2 8.3 ,8.4"
 read -p "Enter the PHP version you want to install (e.g., 8.2): " php_version
 
 # Validate PHP version input
-if [[ ! "$php_version" =~ ^8\.[0-2]$ ]]; then
+if [[ ! "$php_version" =~ ^8\.[0-4]$ ]]; then
 	echo "Invalid PHP version. Please run the script again and enter a valid version."
 	exit 1
 fi
@@ -38,8 +38,8 @@ sudo apt install -y \
     php$php_version-zip php$php_version-bz2 php$php_version-fpm php$php_version-sqlite3 \
     php$php_version-imagick php$php_version-bcmath php$php_version-soap php$php_version-igbinary \
     php$php_version-redis php$php_version-memcached php$php_version-apcu php$php_version-mongodb \
-    php$php_version-memcache php$php_version-pgsql php$php_version-sodium php$php_version-geoip \
-    php$php_version-ldap php$php_version-swoole php$php_version-openssl
+    php$php_version-memcache php$php_version-pgsql \
+    php$php_version-ldap php$php_version-swoole
 
 # Add Node.js repository and install Node.js 20
 echo "Adding Node.js 20 repository..."
